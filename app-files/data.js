6050aa27e5d6431f7a48e3398b62ae369740e73e
Danny971023 <f950933@gmail.com>
2024-11-01 11:31:11 +0800

    1

---
+++ app-files/data.js
@@ -0,0 +1,344 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 1.4626858039019934,
+        "pitch": 0.05745189892636837,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2471557498706112,
+          "pitch": 0.11780901518548248,
+          "rotation": 0,
+          "target": "1-1-"
+        },
+        {
+          "yaw": 1.7885489778463493,
+          "pitch": 0.08228332957501117,
+          "rotation": 0,
+          "target": "3-3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.305852963248819,
+          "pitch": -0.04348127352607811,
+          "title": "快龍",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-1-",
+      "name": "展示區1 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.6179890310632921,
+        "pitch": 0.06869504721209907,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.048683109255424,
+          "pitch": 0.15167291173791853,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.0517057586970573,
+          "pitch": 0.19980324558450846,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": 0.31524349754936587,
+          "pitch": 0.25061690527913427,
+          "rotation": 0,
+          "target": "3-3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4202066620675673,
+          "pitch": -0.13631065873626724,
+          "title": "皮卡丘",
+          "text": ""
+        },
+        {
+          "yaw": -2.204915032745223,
+          "pitch": 0.5248664919577593,
+          "title": "新葉貓",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-2",
+      "name": "展示區2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 1.4901812076976872,
+        "pitch": -0.05985826881313372,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0383636051219938,
+          "pitch": 0.37999804669576953,
+          "rotation": 0,
+          "target": "1-1-"
+        },
+        {
+          "yaw": 0.7722432068306571,
+          "pitch": 0.27534739416327625,
+          "rotation": 0,
+          "target": "3-3-"
+        },
+        {
+          "yaw": -0.2062561375707581,
+          "pitch": 0.02338960661763778,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2046910995090059,
+          "pitch": -0.1622605543163047,
+          "title": "新葉貓",
+          "text": ""
+        },
+        {
+          "yaw": 1.6679365581990213,
+          "pitch": -0.4977759037577414,
+          "title": "呆火鱷",
+          "text": ""
+        },
+        {
+          "yaw": 0.994597307625618,
+          "pitch": 0.09123298757416975,
+          "title": "水潤鴨",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-3-",
+      "name": "展示區3 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.1848606840422207,
+        "pitch": 0.2518615699094564,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1225192071806873,
+          "pitch": 0.489944664594109,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": 1.835256868097714,
+          "pitch": 0.47669398060945767,
+          "rotation": 0,
+          "target": "1-1-"
+        },
+        {
+          "yaw": 1.4961049985199137,
+          "pitch": 0.09068281602202788,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.0328956439933705,
+          "pitch": 0.09962430542290157,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.741330451228534,
+          "pitch": -0.0788151912704258,
+          "title": "水潤鴨",
+          "text": ""
+        },
+        {
+          "yaw": 2.488594090965373,
+          "pitch": 0.17034922225527183,
+          "title": "呆火鱷",
+          "text": ""
+        },
+        {
+          "yaw": -0.350390486478549,
+          "pitch": 0.9640097021938754,
+          "title": "新葉貓",
+          "text": ""
+        },
+        {
+          "yaw": 0.42293786680852463,
+          "pitch": 0.6645026279039161,
+          "title": "呆火鱷",
+          "text": ""
+        },
+        {
+          "yaw": 2.2215551124967607,
+          "pitch": 0.46689182315716415,
+          "title": "新葉貓",
+          "text": ""
+        },
+        {
+          "yaw": -0.4342792681412746,
+          "pitch": 0.666891958881866,
+          "title": "水潤鴨",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "卡牌區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6387464335026927,
+          "pitch": 0.09234221165923451,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": 0.38290541447456405,
+          "pitch": 0.08656267093241965,
+          "rotation": 0,
+          "target": "3-3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.804672534525645,
+          "pitch": 0.4062252816738088,
+          "title": "卡牌區",
+          "text": ""
+        },
+        {
+          "yaw": -0.9294098153417263,
+          "pitch": 0.40277355119193814,
+          "title": "卡牌",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
