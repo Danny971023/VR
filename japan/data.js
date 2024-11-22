65b66a13a3e83d63033d180641b7a391519e132e
Danny971023 <f950933@gmail.com>
2024-11-22 11:42:04 +0800

    1

---
+++ japan/data.js
@@ -0,0 +1,170 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6313393474414752,
+          "pitch": 0.13567961901597592,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 0.45506322321334025,
+          "pitch": 0.140236590636718,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3138599285390349,
+          "pitch": 0.5197768116217425,
+          "title": "卡牌",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": 1.2107807181024661,
+        "pitch": 0.17359837686561974,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.515196358156956,
+          "pitch": 0.2483021116726576,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 2.260412257524175,
+          "pitch": 0.14850184943519196,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.17778189227294305,
+          "pitch": -0.04599975120505562,
+          "title": "喵喵",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -2.3963602309370984,
+        "pitch": 0.15665401920128552,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0673235663929805,
+          "pitch": 0.15300796054312293,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -1.69958044156186,
+          "pitch": 0.14881664973571773,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.099654437969324,
+          "pitch": -0.011296586643425144,
+          "title": "超夢",
+          "text": ""
+        },
+        {
+          "yaw": -2.936300470926806,
+          "pitch": 0.24075084089895782,
+          "title": "路卡利歐",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
